5b4c83584bd69302a6dd9cd653e93b64df8cad4a
Raymond SO <rso@ouhk.edu.hk>
2015-12-09 12:18:09 +0800

    a naive example of implementing authentication using Express session

---
+++ login/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var SECRETKEY = 'I want to pass COMPS381F';
+var users = new Array(
+	{name: 'developer', password: 'developer'},
+	{name: 'guest', password: 'guest'}
+);
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+	secret: SECRETKEY,
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.get('/',function(req,res) {
+	console.log(req.session);
+	if (!req.session.authenticated) {
+		res.redirect('/login');
+	}
+	res.end('This is a secret page!');
+});
+
+app.get('/login',function(req,res) {
+	res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/login',function(req,res) {
+	for (var i=0; i<users.length; i++) {
+		if (users[i].name == req.body.name && 
+		    users[i].password == req.body.password) {
+			req.session.authenticated = true;
+		}
+	}
+	res.redirect('/');
+});
+
+app.get('/logout',function(req,res) {
+	req.session.destroy();
+	res.redirect('/');
+});
+
+app.listen(process.env.PORT || 8099);
