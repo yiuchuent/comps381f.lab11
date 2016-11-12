a91c1c16229a6e0194bc400980c394c515a6c7ea
Raymond So <rso@ouhk.edu.hk>
2016-11-12 13:07:09 +0800

    Template for shopping cart

---
+++ shoppingcart/server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fileUpload = require('express-fileupload');
+
+var SECRETKEY = 'I want to pass COMPS381F';
+
+app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(session({
+	secret: SECRETKEY,
+	resave: true,
+	saveUninitialized: true
+}));
+
+var products = [
+	{name: 'Apple iPad Pro', stock: 100, price: 7000, id:'001'},
+	{name: 'Apple iPhone 7', stock: 50, price: 7800, id:'002'},
+	{name: 'Apple Macbook', stock: 70, price: 11000, id: '003'}
+];
+
+app.set('view engine', 'ejs');
+
+app.get("/read", function(req,res) {
+	res.render("list", {c: products});
+});
+
+app.get('/showdetails', function(req,res) {
+	if (req.query.id != null) {
+		for (var i=0; i<products.length; i++) {
+			if (products[i].id == req.query.id) {
+				var product = products[i];
+				break;
+			}
+		}
+		if (product != null) {
+			res.render('details', {c: product});
+		} else {
+			res.status(500).end(req.query.id + ' not found!');
+		}
+	} else {
+		res.status(500).end('id missing!');
+	}
+});
+
+app.get('/shoppingcart', function(req,res) {
+	res.end('coming soon!')
+});
+
+app.get('/add2cart', function(req,res) {
+	res.end('coming soon!')
+})
+
+app.get('/emptycart',function(req,res) {
+	res.end('coming soon!')
+})
+
+
+app.listen(process.env.PORT || 8099);
